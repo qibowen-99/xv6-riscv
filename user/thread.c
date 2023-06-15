1598878e182e2dfa961f480b33b583d57e056be8
BowenZzz1 <77997193+BowenZzz1@users.noreply.github.com>
2023-06-14 23:11:44 -0700

    Update thread.c

--- user/thread.c
+++ user/thread.c
@@ -32,13 +32,13 @@ int thread_create(void *(start_routine)(void*), void *arg) {
 
 
 void lock_init(lock_t* lock){
-    lock->locked = 0;
+    lock->locked = 0; 
 }
 
 void lock_acquire(lock_t* lock){
-    while(__sync_lock_test_and_set(&lock->locked, 1) != 0) {}; 
+    while(__sync_lock_test_and_set(&lock->locked, 1) != 0) {};  
 }
 
 void lock_release(lock_t* lock){
-    __sync_lock_release(&lock->locked);
+    __sync_lock_release(&lock->locked); 
 }
